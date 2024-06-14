9208c122db997589583187bbdeec59ac4d507daf
songsu <jole_wong@outlook.com>
2024-06-15 00:39:34 +0800

    修复fuel领水

--- src/fuel/fuelToken.js
+++ src/fuel/fuelToken.js
@@ -1,39 +1,41 @@
 const axios = require('axios');
 const fs = require('fs');
 const csv = require('csv-parser');
+const readline = require('readline'); 
 const config = require('../../config/runner.json');
 const { HttpsProxyAgent } = require('https-proxy-agent');
 const fakeUa = require('fake-useragent');
 const userAgent = fakeUa();
-const { sleep, randomPause, sendRequest} = require('../../utils/utils.js');
-const { createTask, getTaskResult } = require('../../utils/yesCaptcha/yesCaptcha.js');
+const { sleep, randomPause, sendRequest, logger} = require('../../utils/utils.js');
+const { createTask, getTaskResult } = require('../../utils/capsolver/capsolver.js');
+const pLimit = require('p-limit');
 
 const MAX_RETRIES = 5; // 最大重试次数
 const MAX_PROXY_CHECK_ATTEMPTS = 3;
+const CONCURRENCY_LIMIT = 20; // 并发限制
 
 const agent = new HttpsProxyAgent(config.proxy);
 const websiteKey = '6Ld3cEwfAAAAAMd4QTs7aO85LyKGdgj0bFsdBfre';
-const websiteUrl = 'https://faucet-beta-5.fuel.network/';
+const websiteUrl = 'https://faucet-testnet.fuel.network/dispense';
 headers = {
     'authority': 'faucet-beta-5.fuel.network', 
     'accept': '*/*',
     'Accept-Encoding': 'gzip, deflate, br',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',  
     'content-type': 'application/json',
-    'origin': 'https://faucet-beta-5.fuel.network', 
-    'referer': 'https://faucet-beta-5.fuel.network',
+    'origin': 'https://faucet-testnet.fuel.network', 
+    'referer': 'https://faucet-testnet.fuel.network/',
     'user-agent': userAgent,
 }
 
-async function recaptcha(pageAction) {
-    const {taskId} = await createTask(websiteUrl, websiteKey, 'NoCaptchaTaskProxyless', pageAction);
+async function recaptcha( ) {
+    const {taskId} = await createTask(websiteUrl, websiteKey, 'RecaptchaV2EnterpriseTaskProxyless');
     let result = await getTaskResult(taskId);
-    // 如果result为空，等待6秒后再次请求
     if (!result) {
         await sleep(0.1);
         result = await getTaskResult(taskId);
     }
-    // 如果再次为空，抛出错误
+
     if (!result) {
         throw new Error(`${pageAction} 人机验证失败`);
     }
@@ -41,25 +43,20 @@ async function recaptcha(pageAction) {
     return gRecaptchaResponse
 }
 
-async function processAddresses(filePath) {
+async function processAddresses() {
     const addresses = [];
-    return new Promise((resolve, reject) => {
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (row) => {
-                addresses.push(row.fuelAddress);
-            })
-            .on('end', () => {
-                console.log('地址读取完毕');
-                resolve(addresses);
-            })
-            .on('error', (error) => {
-                console.error('读取地址失败:', error);
-                reject(error);
-            });
+    const fileStream = fs.createReadStream('addr.txt');
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
     });
-}
 
+    for await (const line of rl) {
+        addresses.push(line.trim());
+    }
+
+    return addresses;
+}
 
 async function main() {
     try {
@@ -75,50 +72,57 @@ async function main() {
                     httpsAgent: agent
                 });
                 console.log('验证成功, IP信息: ', response);
-                proxyVerified = true; // 代理验证成功
+                proxyVerified = true;
             } catch (error) {
                 proxyAttempts++;
                 console.log('代理失效，等待1分钟后重新验证');
-                await sleep(60); // 等待1分钟
+                await sleep(60);
             }
         }
 
         if (!proxyVerified) {
             console.log('代理验证失败，无法继续执行任务');
-            return; // 如果代理验证失败，结束函数
+            return; 
         }
     } catch (error) {
         console.error('发生错误:', error);
     }
 
-    const fuelAddresses = await processAddresses(config.walletPath);
+    const fuelAddresses = await processAddresses();
     console.log('开始领取测试币');
 
-    for (const fuelAddress of fuelAddresses) {
-        console.log(`领取地址: ${fuelAddress}`);
-        let attempts = 0;
-        let success = false;
-        while (attempts < MAX_RETRIES && !success) {
-            try {
-                const recaptchaToken = await recaptcha('giveMeEther');
-                const response = await claimTestCoins(fuelAddress, recaptchaToken); 
-                if (response && response.status === 'Success') {
-                    console.log(`领取成功✅，地址：${fuelAddress}，领取数量：${response.tokens}`);
-                    success = true;
-                } else {
-                    throw new Error(`服务器返回失败状态: ${response.data.status}`);
+    const limit = pLimit(CONCURRENCY_LIMIT);
+
+    const tasks = fuelAddresses.map(fuelAddress => 
+        limit(async () => {
+            console.log(`领取地址: ${fuelAddress}`);
+            let attempts = 0;
+            let success = false;
+            while (attempts < MAX_RETRIES && !success) {
+                try {
+                    const recaptchaToken = await recaptcha();
+                    const response = await claimTestCoins(fuelAddress, recaptchaToken); 
+                    if (response && response.status === 'Success') {
+                        console.log(`领取成功✅，地址：${fuelAddress}，领取数量：${response.tokens}`);
+                        success = true;
+                    } else {
+                        throw new Error(`服务器返回失败状态: ${response.data.status}`);
+                    }
+                } catch (error) {
+                    console.error(`领取失败❌，地址：${fuelAddress}，错误：${error.message}, 开始重试第 ${attempts + 1} 次`);
+                    await sleep(5); 
                 }
-            } catch (error) {
-                console.error(`领取失败❌，地址：${fuelAddress}，错误：${error.message}, 开始重试第 ${attempts + 1} 次`);
-                await sleep(5); 
+                attempts++;
             }
-            attempts++;
-        }
-    }
+        })
+    );
+
+    await Promise.all(tasks);
+    console.log('所有任务已完成');
 }
 
 async function claimTestCoins(fuelAddress, recaptchaToken) {
-    const url = `https://faucet-beta-5.fuel.network/dispense`;
+    const url = `https://faucet-testnet.fuel.network/dispense`;
     const data = { 
         address: fuelAddress,
         captcha: recaptchaToken,
